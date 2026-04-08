1d9ee06369a9fc88147ebab008ea06413850fcfc
JhowQT <jhon.q.torrez@gmail.com>
2026-04-08 09:37:39 -0300

    ajutes na procedure para se adaptar no clob

--- sql/sprint 3/prc_relatorio_sequencial.sql
+++ sql/sprint 3/prc_relatorio_sequencial.sql
@@ -3,7 +3,9 @@ AS
     v_count NUMBER;
 BEGIN
 
-    SELECT COUNT(*) INTO v_count FROM T_OR_COMENTARIO;
+    SELECT COUNT(*) 
+    INTO v_count 
+    FROM T_OR_COMENTARIO;
 
     IF v_count < 5 THEN
         RAISE_APPLICATION_ERROR(-20070, 'Quantidade insuficiente de dados');
@@ -12,9 +14,21 @@ BEGIN
     FOR rec IN (
         SELECT 
             id_comentario,
-            NVL(DBMS_LOB.SUBSTR(LAG(cd_comentario) OVER (ORDER BY id_comentario), 4000, 1), 'Vazio') AS anterior,
+
+            NVL(
+                LAG(DBMS_LOB.SUBSTR(cd_comentario, 4000, 1)) 
+                OVER (ORDER BY id_comentario),
+                'Vazio'
+            ) AS anterior,
+
             DBMS_LOB.SUBSTR(cd_comentario, 4000, 1) AS atual,
-            NVL(DBMS_LOB.SUBSTR(LEAD(cd_comentario) OVER (ORDER BY id_comentario), 4000, 1), 'Vazio') AS proximo
+
+            NVL(
+                LEAD(DBMS_LOB.SUBSTR(cd_comentario, 4000, 1)) 
+                OVER (ORDER BY id_comentario),
+                'Vazio'
+            ) AS proximo
+
         FROM T_OR_COMENTARIO
     ) LOOP
 
@@ -30,9 +44,12 @@ BEGIN
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('Sem dados');
+
     WHEN VALUE_ERROR THEN
         DBMS_OUTPUT.PUT_LINE('Erro de valor');
+
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro geral: ' || SQLERRM);
+
 END;
 /
